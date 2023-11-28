4f0ebdeb380b72af0d744a43b250ef57dada6f36
Gabriel <gabriel.alves9400@hotmail.com>
2023-11-28 18:41:14 -0300

    teste

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { ListComponent} from "./views/list/list.component";
+import {CreateComponent} from "./views/create/create.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: ListComponent
+  },
+  {
+    path:'editar/:id', component: CreateComponent
+  },
+  {
+    path: 'criar', component: CreateComponent
+  },
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  }
+];
